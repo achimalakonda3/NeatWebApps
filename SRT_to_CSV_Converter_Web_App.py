86cccefda182db9db6d03b2c4b0b1f237f85e850
Adithya <achimalakonda3@gatech.edu>
2025-02-26 17:04:13 -0500

    added line to drone path display

--- SRT_to_CSV_Converter_Web_App.py
+++ SRT_to_CSV_Converter_Web_App.py
@@ -75,7 +75,7 @@ def render_svg(svg):
     """Renders the given svg string."""
     b64 = base64.b64encode(svg.encode('utf-8')).decode("utf-8")
     html = r'<img src="data:image/svg+xml;base64,%s"/>' % b64
-    st.write(html, unsafe_allow_html=True)
+    col_1.write(html, unsafe_allow_html=True)
 
 def create_svg_from_coordinates(coordinates, dot_size=5, filename='path.svg'):
     if not coordinates.any():
@@ -99,22 +99,29 @@ def create_svg_from_coordinates(coordinates, dot_size=5, filename='path.svg'):
 
     path_coords = [(int(scale * (x - min_x))+dot_size, int(scale * (y - min_y))+dot_size) for x, y in relative_coords]
 
-    height = int(scale* (max_y-min_y)) + 2*dot_size
+    height = int(scale* (max_y-min_y)) + 2*dot_size + 50
     width = int(scale * (max_x-min_x)) + 2*dot_size
     
     # Create SVG
     map_to_display = f'<?xml version="1.0" encoding="utf-8"?>'
     map_to_display += f'\n<svg width="{width}px" height="{height}px" version="1.1" xmlns="http://www.w3.org/2000/svg">'
+    # Graph all the points except first and last
     map_to_display += f'<g id="path_points">'
-    for x, y in path_coords:
-        map_to_display += f'\n    <circle cx="{x}" cy="{y}" r="{dot_size}" fill="blue" />'
+    for x, y in path_coords[1:-1]:
+        map_to_display += f'\n    <circle cx="{x}" cy="{y}" r="{dot_size}" fill="red" />'
     map_to_display += f'</g>'
+    # Add a line:
+    map_to_display += f'\n    <polyline points="'
+    for x, y in path_coords:
+        map_to_display += f'{x},{y} '
+    map_to_display += f'" \n    style="fill:none; stroke:blue; stroke-width:{dot_size/2}" />'
+
     # Add start point to a group
-    map_to_display += f'<g id="start_point">'
+    map_to_display += f'\n<g id="start_point">'
     map_to_display += f'\n    <circle cx="{path_coords[0][0]}" cy="{path_coords[0][1]}" r="{2*dot_size}" fill="green" />'
     map_to_display += f'</g>'
     # Add end point to a group
-    map_to_display += f'<g id="end_point">'
+    map_to_display += f'\n<g id="end_point">'
     map_to_display += f'\n    <circle cx="{path_coords[-1][0]}" cy="{path_coords[-1][1]}" r="{2*dot_size}" fill="red" />'
     map_to_display += f'</g>'
     map_to_display += f'\n</svg>'
@@ -133,7 +140,9 @@ st.markdown('###')
 
 
 # File upload
-uploaded_file = st.file_uploader("Upload your SRT file", type=["srt"])
+col_1, col_2 = st.columns([0.7, 0.3])
+uploaded_file = col_1.file_uploader("Upload your SRT file", type=["srt"])
+
 
 if uploaded_file is not None:
     # Read file content
@@ -145,18 +154,18 @@ if uploaded_file is not None:
     path_svg = create_svg_from_coordinates(coordinates)
 
     # Download SVG
-    file_name = st.text_input("Name SVG File", value="path")
-    st.download_button('Download Path as SVG', data=path_svg, file_name=f'{file_name}.svg')
+    file_name = col_1.text_input("Name SVG File", value="path")
+    col_1.download_button('Download Path as SVG', data=path_svg, file_name=f'{file_name}.svg')
 
     # Display the DataFrame
-    st.subheader("Parsed Data")
-    st.dataframe(map_data)
+    col_2.subheader("Parsed Data")
+    col_2.dataframe(map_data)
 
     # Download CSV
     csv_buffer = BytesIO()
     map_data.to_csv(csv_buffer, index=False)
     csv_buffer.seek(0)
-    st.download_button(
+    col_2.download_button(
         label="Download CSV",
         data=csv_buffer,
         file_name=f"{uploaded_file.name[:-4]}.csv",
